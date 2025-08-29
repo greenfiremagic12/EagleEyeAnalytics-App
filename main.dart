98df6f32ea062110a029219c7e6607f3ee5f59a1
greenfiremagic12 <bradmanonfire@gmail.com>
2025-08-29 16:49:24 -0500

    dart basics

---
+++ main.dart
@@ -0,0 +1,24 @@
+import "dart:io";
+
+void main() {
+
+  int int_t;
+  double double_t;
+  num num_t;
+  String String_t;
+  bool bool_t;
+  var var_t;
+
+
+  var var_r = (2, 2);
+  var var_r2 = (a: 2, b: 2);
+
+  (int, int) var_r_a = (2, 2);
+  ({int a, int b}) var_r2_a = (a: 1, b: 2);
+
+  print("$var_r2_a");
+
+  var list = [1, 2, 3]; 
+
+
+}
